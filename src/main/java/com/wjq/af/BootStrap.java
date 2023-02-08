b87152e3041c324c3681045dc17070b1189aec22
zengsitong <zengsitong@newhope.cn>
2023-02-08 16:36:02 +0800

    Initial commit

---
+++ src/main/java/com/wjq/af/BootStrap.java
@@ -0,0 +1,13 @@
+package com.wjq.af;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalFundApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run (AnimalFundApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/wjq/af/BootStrapTests.java
@@ -0,0 +1,13 @@
+package com.wjq.af;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalFundApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
