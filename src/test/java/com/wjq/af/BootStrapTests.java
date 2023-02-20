7ecb068f1239be303427d24fc134420f723fd84e
zengsitong <zengsitong@newhope.cn>
2023-02-20 10:06:07 +0800

    优化多环境配置

--- src/test/java/com/wjq/af/BootStrapTests.java
+++ src/test/java/com/wjq/af/BootStrapTests.java
@@ -1,13 +1,22 @@
 package com.wjq.af;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BootStrapTests {
     
+    @Value ("${animal.name}")
+    private String name;
+    
+    @Value ("${animal.redis.db}")
+    private Integer redisDb;
+    
     @Test
     void contextLoads() {
+        System.out.println (name);
+        System.out.println (redisDb);
     }
     
 }
