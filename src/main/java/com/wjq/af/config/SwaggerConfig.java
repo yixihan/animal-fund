41f616663aaadb241d170c674aaa42018aacd319
zengsitong <zengsitong@newhope.cn>
2023-02-20 15:41:30 +0800

    去除 swagger doc 页面基础接口展示

--- src/main/java/com/wjq/af/config/SwaggerConfig.java
+++ src/main/java/com/wjq/af/config/SwaggerConfig.java
@@ -1,13 +1,17 @@
 package com.wjq.af.config;
 
+import com.wjq.af.auth.prop.AuthProp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.Resource;
 
 /**
  * Swagger 配置类
@@ -19,12 +23,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
     
+    @Resource
+    private AuthProp prop;
+    
     /**
      * 配置 Swagger 的 Docket 实例
      */
     @Bean
     public Docket docket() {
         return new Docket (DocumentationType.OAS_30)
+                .select ()
+                .apis (RequestHandlerSelectors.basePackage (prop.getControllerPackage ()))
+                .build ()
                 // 创建该 API 的基本信息
                 .apiInfo (apiInfo ());
         
